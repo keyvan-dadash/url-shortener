b41255d920313047d7f607781e398d7d378753b4
keyvan1379 <ddssee5511@gmail.com>
2021-08-15 08:28:17 -0400

    main file added

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"context"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sod-lol/url-shortener/core/repos/url_repo"
+	"github.com/sod-lol/url-shortener/routers"
+	"github.com/sod-lol/url-shortener/services/redis"
+)
+
 func main() {
 
+	router := gin.New()
+
+	root := context.Background()
+
+	redisClient := redis.CreateRedisClient("redis-auth:6379", "", 0)
+	defer redisClient.Close()
+	urlRepo := url_repo.URLRedisStorage{
+		Client: redisClient,
+	}
+
+	ctxWithRepo := context.WithValue(root, "user-repo", urlRepo)
+
+	routers.InitRoutes(ctxWithRepo, &router.RouterGroup)
+
+	router.Run(":8080")
 }
--- routers/index.go
+++ routers/index.go
@@ -1,11 +1,13 @@
 package routers
 
 import (
+	"context"
+
 	"github.com/gin-gonic/gin"
 	"github.com/sod-lol/url-shortener/routers/url_r"
 )
 
-func InitRoutes(g *gin.RouterGroup) {
+func InitRoutes(ctx context.Context, g *gin.RouterGroup) {
 
-	url_r.SetUpUrlRoutes(g.Group("/url"))
+	url_r.SetUpUrlRoutes(ctx, g.Group("/url"))
 }
