fed3311745cc4397859794c4aab988d4c49d537d
keyvan1379 <ddssee5511@gmail.com>
2021-08-15 08:16:11 -0400

    handlers added and url routers completed

---
+++ controllers/url_c/url_handlers.go
@@ -0,0 +1,73 @@
+package url_c
+
+import (
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sod-lol/url-shortener/core/models/url_model"
+	"github.com/sod-lol/url-shortener/core/repos/url_repo"
+)
+
+func HandlerRedirect(urlRepo url_repo.URLRepo) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		shortUrl := c.Param("shortUrl")
+		ctx := c.Request.Context()
+		url, err := urlRepo.GetURLByShortURL(ctx, shortUrl)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		url.Clicked += 1
+		err = urlRepo.SaveURL(ctx, url)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		c.Redirect(http.StatusMovedPermanently, url.OriginalURL)
+
+	}
+}
+
+type shortURLRequest struct {
+	URL string `form:"url" json:"url" xml:"url"  binding:"required"`
+}
+
+func HandlerShortURLRequest(urlRepo url_repo.URLRepo) gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		var urlRequestJson shortURLRequest
+
+		if err := c.ShouldBindJSON(&urlRequestJson); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		ctx := c.Request.Context()
+		url := url_model.CreateURLObj(rand.Uint64(), urlRequestJson.URL, time.Hour*2)
+
+		err := urlRepo.SaveURL(ctx, url)
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"err": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusAccepted, gin.H{
+			"short_url": url.ShortURL,
+		})
+
+	}
+}
--- routers/url_r/url_r.go
+++ routers/url_r/url_r.go
@@ -1,9 +1,16 @@
 package url_r
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
 
-func SetUpUrlRoutes(g *gin.RouterGroup) {
+	"github.com/gin-gonic/gin"
+	"github.com/sod-lol/url-shortener/controllers/url_c"
+	"github.com/sod-lol/url-shortener/core/repos/url_repo"
+)
 
-	g.POST("/submit/")
-	g.GET("/{shortUrl}/")
+func SetUpUrlRoutes(ctx context.Context, g *gin.RouterGroup) {
+
+	userRepo := ctx.Value("user-repo").(url_repo.URLRepo)
+	g.POST("/submit", url_c.HandlerShortURLRequest(userRepo))
+	g.GET("/:shortUrl/", url_c.HandlerRedirect(userRepo))
 }
